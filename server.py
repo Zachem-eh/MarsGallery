9fa4b9a67c7dca7eb7334b5fec4c680682196a3e
Zachem-eh <vladsasha7002@gmail.com>
2025-05-17 13:18:31 +0300

    add photo

--- server.py
+++ server.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import SubmitField, FileField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum'
 
-@app.route('/carousel')
+
+class LoadForm(FlaskForm):
+    image = FileField('Приложите фотографию',
+                      validators=[DataRequired()],
+                      render_kw={'accept': 'image/*'})
+    submit = SubmitField('Отправить')
+
+
+@app.route('/carousel', methods=['GET', 'POST'])
 def mars_gallery():
-    return render_template('index.html')
+    form = LoadForm()
+    images = [url_for('static', filename=f'images/{file}') for file in os.listdir('static/images')]
+    print(images)
+    if form.validate_on_submit():
+        file = form.image.data
+        filename = f'mars{len(os.listdir("static/images")) + 1}.jpg'
+        save_path = os.path.join('static/images', filename)
+        file.save(save_path)
+        return redirect('/carousel')
+    return render_template('index.html', form=form,  images=images)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
